f78e61f423969f6ffe937f111e5ac4e244973a23
Shadrach Odekhiran <shadrach.odekhiran@gmail.com>
2020-01-31 03:47:22 +0100

    Logger implementation base on PSR-3

---
+++ Src/Exception/InvalidLogLevelArgument.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Shadrach Odekhiran
+ * Date: 1/31/2020
+ * Time: 03:25
+ */
+
+namespace App\Exception;
+
+
+class InvalidLogLevelArgument extends BaseException
+{
+
+}
---
+++ Src/Logger/LogLevel.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Shadrach Odekhiran
+ * Date: 1/31/2020
+ * Time: 01:27
+ */
+
+namespace App\Logger;
+
+
+class LogLevel
+{
+    const DEBUG = 'debug';
+    const INFO = 'info';
+    const NOTICE = 'notice';
+    const WARNING = 'warning';
+    const ERROR = 'error';
+    const CRITICAL = 'critical';
+    const ALERT = 'alert';
+    const EMERGENCY = 'emergency';
+}
